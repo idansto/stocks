bacb7fbdcbcc1c69b4a6f815069e75b516bfd263
Ariel <ariel.stollman@gmail.com>
2020-10-03 23:28:06 +0300

    hi

---
+++ src/databuilder/bl/GlobalMetricBuilder.py
@@ -0,0 +1,18 @@
+from csv import reader
+
+
+def populate_global_metric_data():
+    pass
+
+def populate_Federal_Funds_Rate():
+    # (connection, cursor) = get_connection_cursor()
+    with open('../resources/fed-funds-rate-historical-chart.csv', 'r') as file:
+        csv_reader = reader(file)
+        for row in csv_reader:
+            (date, value) = row
+            sql = "insert (date, metric_name, value) into shares.global_data values (%s, %s, %s)"
+            values = (date, 1, value)
+            # print(sql, values)
+    #         cursor.execute(sql)
+    # connection.commit
+    return
