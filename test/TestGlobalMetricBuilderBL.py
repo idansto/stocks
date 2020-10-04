53650ab80b9908dbc083b4297c608e527d63a4e8
Ariel <ariel.stollman@gmail.com>
2020-10-04 12:08:42 +0300

    hi

--- test/TestGlobalMetricBuilderBL.py
+++ test/TestGlobalMetricBuilderBL.py
@@ -1,6 +1,7 @@
 import unittest
 
-from databuilder.bl.GlobalMetricBuilderBL import populate_federal_funds_rate_from_json
+from databuilder.bl.GlobalMetricBuilderBL import populate_federal_funds_rate_from_json, populate_federal_funds_rate_TRY, \
+    populate_10_year_treasury_rate_from_json
 
 
 class TestGlobalMetricBuilderBl(unittest.TestCase):
@@ -8,3 +9,8 @@ class TestGlobalMetricBuilderBl(unittest.TestCase):
     def test_populate_federal_funds_rate_from_json(self):
         populate_federal_funds_rate_from_json()
 
+    def test_populate_federal_funds_rate(self):
+        populate_federal_funds_rate_TRY()
+
+    def test_populate_10_year_treasury_rate_from_json(self):
+        populate_10_year_treasury_rate_from_json()
